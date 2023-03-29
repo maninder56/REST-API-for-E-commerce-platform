d1759c4c79866cc48eb4a9b6ac49e31c0d03519b
Maninder singh <singhm50@coventry.ac.uk>
2023-03-29 19:16:03 +0000

    implemented function to run SQL queries

---
+++ helpers/database.js
@@ -0,0 +1,16 @@
+const mysql = require('promise-mysql');
+const info = require('../config');
+
+// run SQL query then end the connection 
+exports.run_query = async function run_query(query, values) {
+    try {
+        const connection = await mysql.createConnection(info.config);
+        let data = await connection.query(query, values);
+        await data;
+    } catch (error){
+        // log error and give generic error message
+        console.error(error, query, values);
+        throw 'Database query error' 
+    }
+
+}
