1876dd30c80dd8b1be411380460bb301e7ec78d8
Maninder singh <singhm50@coventry.ac.uk>
2023-03-30 09:52:50 +0000

    added user model and user resource

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const app = new Koa();
 
 const special = require('./routes/special.js');
 const products = require('./routes/products.js');
+const users = require('./routes/users');
 
 app.use(special.routes());
 app.use(products.routes());
+app.use(users.routes());
 
 let port = process.env.PORT || 3000
 app.listen(port);
---
+++ routes/users.js
@@ -0,0 +1,68 @@
+const Router = require('koa-router');
+const bodyParser =require('koa-bodyparser');
+const model = require('../models/users');
+
+const router = Router({prefix: '/api/v1/users'})
+
+// All endpoints related to product
+router.get('/', getAll);
+router.post('/', bodyParser(), createUser); // bodyparser is needed to retrieve data from client 
+
+router.get('/:id([0-9]{1,})', getById); 
+router.put('/:id([0-9]{1,})', bodyParser(), updateUser); 
+router.del('/:id([0-9]{1,})', deleteUser);
+
+// Get all users 
+async function getAll(ctx, next){
+    let products = await model.getAll();
+    if (products.length){
+        ctx.body = products;
+    }
+}
+
+// Get user by ID 
+async function getById(ctx, next){
+    let id = ctx.params.id;
+    console.log( "Requested User ID: ",id);
+    let user = await model.getByID(id);
+    if (user.length){
+        ctx.body = user[0]
+    }
+}
+
+// create user 
+async function createUser(ctx, next){
+    const body = ctx.request.body;
+    let result = await model.createUser(body);
+    if (result){
+        ctx.status = 201;
+        ctx.body = {ID: result.insertId} // return ID of User
+    }
+}
+
+// Update Product using product ID 
+async function updateUser(ctx, next){
+    let id = ctx.params.id;
+    const body = ctx.request.body;
+    console.log("Recieved values:\n",id,"\n",body)
+    let update = await model.updateUser(id, body);
+    if (update){
+        ctx.status = 201;
+        ctx.body = update;
+    }
+}
+
+// Delete products by product id 
+async function deleteUser(ctx, next){
+    let id = ctx.params.id;
+    console.log("Deleted U=user_id: ",id)
+    let deleteUser = await model.deleteUser(id);
+    if (deleteUser){
+        ctx.status = 202;
+        ctx.body = deleteUser;
+    }
+}
+
+module.exports = router;
+
+
