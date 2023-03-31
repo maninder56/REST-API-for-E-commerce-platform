792dea6c4b9dfd7963c4a3a986bce5baa133af25
Maninder singh <singhm50@coventry.ac.uk>
2023-03-31 13:59:15 +0000

    added products schema and validation midware

---
+++ controllers/validation.js
@@ -0,0 +1,27 @@
+const {Validator, ValidationError} = require('jsonschema');
+
+const schema = require('../schemas/products.schema');
+
+const v = new Validator();
+
+exports.validateArticle = async (ctx, next) => {
+    
+    const validationOptions = {
+        throwError: true,
+        allowUnknownAttributes: false
+    };
+    
+    const body = ctx.request.body;
+    
+    try {
+        v.validate(body, schema, validationOptions);
+        await next();
+    } catch (error) {
+        if (error instanceof ValidationError) {
+            ctx.body = error;
+            ctx.status = 400;
+        } else {
+            throw error;
+        }
+    }
+}
